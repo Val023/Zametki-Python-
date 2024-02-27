27854deec224c2911866297844ff85ed5ae67004
Valentina <vdronova938@gmail.com>
2024-02-27 15:45:26 +0300

    function read title notes

--- zametki.py
+++ zametki.py
@@ -15,4 +15,18 @@ def create_note():
         _file.write(_note)
     
     print(f"Заметка {_title} успешно создана!")
+
+def read_title_notes():
+    if not os.path.exists("Notes"):
+        print("Папки \"Notes\" не существует")
+        return
+    
+    _files = os.listdir("Notes")
+    print("Список заметок:")
+    for _file in _files:
+        with open(f"Notes/{_file}", "r") as _note_file:
+            for _line in _note_file:
+                _split_line = _line.split(";")
+                print(">>: " + _split_line[0])
+                break
     
