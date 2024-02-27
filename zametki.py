6b3f8e7e0c379d7384139e479cfae14d1aa4b365
Valentina <vdronova938@gmail.com>
2024-02-27 15:50:15 +0300

    function edit note

--- zametki.py
+++ zametki.py
@@ -46,3 +46,29 @@ def read_note():
             print(">>: " + _split_line[0])
             print("# : " + _split_line[1])
             break    
+
+def edit_note():
+    if not os.path.exists("Notes"):
+        print("Папки \"Notes\" не существует")
+        return
+    
+    _title = input("Введите название заметки: ")
+    if not os.path.exists(f"Notes/{_title}.json"):
+        print("Заметки не найдена")
+        return
+
+    with open(f"Notes/{_title}.json", "r") as _note_file:
+        for _line in _note_file:
+            _split_line = _line.split(";")
+            print(">>: " + _split_line[0])
+            print("# : " + _split_line[1])
+            break
+            
+    _note = input("Введите новый текст заметки: ")
+    
+    with open(f"Notes/{_title}.json", "w") as _file:
+        _file.write(_title)
+        _file.write(";")
+        _file.write(_note)
+        
+    print(f"Заметка {_title} отредактирована")
